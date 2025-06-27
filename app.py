2a142844778f275baae3253d221e5f86da7ba795
Nakajima-Kenshin <118495686+Nakajima-Kenshin@users.noreply.github.com>
2025-06-27 16:49:34 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -82,6 +82,7 @@ else:
             st.title("📊 個人成績表")
             if os.path.exists("data/成績表.xlsx"):
                 df = pd.read_excel("data/成績表.xlsx", header = 3)
+                st.write(df['名前'].astype(str).str.strip())
                 personal_df = df[df['名前'].astype(str).str.strip() == user]
                 st.dataframe(personal_df, use_container_width=True)
             else:
